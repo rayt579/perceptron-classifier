ca29da472e83f41696fd46d1209835b9bc6cfe3c
Ray <ray@ray.attlocal.net>
2015-06-06 15:49:13 -0700

    working

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,7 @@
 """
 Implementation of Perceptron algorithm on MNIST dataset. 
+Here we use binary classification, distinguishing digits '6' from '0'
+with a linear classifier with averaged and voting decision rules
 """
 
 import numpy as np
@@ -7,7 +9,7 @@ import numpy as np
 
 def dot_prod(v1,v2):
 	'''
-	Returns projection of v1 on v2. 
+	Returns dot product between v1 and v2
 	'''
 	assert v1.size == v2.size, "vectors of different sizes"
 	
@@ -32,14 +34,15 @@ def perceptron(data, labels, n_passes):
 	c_vectors = [1]
 
 	# Iteration step. 
-	for i in xrange(n):
-		if (labels[i] * dot_prod(w,data[i])) <= 0:
-			w = w + (labels[i] * data[i])
-			w_vectors.append(w)
-			c_vectors.append(c)
-			m += 1 
-		else: 
-			c_vectors[m] += 1
+	for x in xrange(n_passes):
+		for i in xrange(n):
+			if (labels[i] * dot_prod(w,data[i])) <= 0:
+				w = w + (labels[i] * data[i])
+				w_vectors.append(w)
+				c_vectors.append(c)
+				m += 1 
+			else: 
+				c_vectors[m] += 1
 
 	weight_mat = np.insert(w_vectors,(data[i].size), c_vectors, axis=1)
 	return weight_mat
@@ -114,21 +117,81 @@ def avg_percep_clf(weight_mat, data):
 
 	return np.array(pred)
 
-def main():
-	# Parse input 
-	train = np.genfromtxt("toy_data/toy_train.txt")
-	test = np.genfromtxt("toy_data/toy_test.txt")
+def calc_error(true_labels, pred_labels):
+	'''
+	Returns percent error of predicted labels
+	'''
+	assert true_labels.size == pred_labels.size
+	n = true_labels.size
 
-	n_feat = train[0].size
+	# Calculate the error
+	mis_count = 0.0
+	for i in xrange(n):
+		if pred_labels[i] != true_labels[i]:
+			mis_count += 1.0
+	return (mis_count/n)
+
+def convert_labels(labels):
+	'''
+	Returns binary labels, where 0 is '1' and 6 is '-1'
+	'''
+	new_labels = []
+	for point in labels: 
+		if point == 0.0:
+			new_labels.append(1.0)
+		else: 
+			new_labels.append(-1.0)
+	return np.array(new_labels)
+
+def main():
+	'''
+	Gets the test and training error of perceptron, voted perceptron, and 
+	averaged perceptron at 1, 2, and 3 passes.
+	'''
+	# Parse input 
+	train = np.genfromtxt("data/hw4atrain.txt")
+	test = np.genfromtxt("data/hw4atest.txt")
+
 	# Separate data from labels
+	n_feat = train[0].size
 	train_labels = train[:,n_feat-1]
 	train_data = train[:,:-1]
 	test_labels = test[:,n_feat-1]
 	test_data = test[:,:-1]
 
+	# Convert labels to 1 and -1 for perceptron algorithm
+	new_train_labels = convert_labels(train_labels)
+	new_test_labels = convert_labels(test_labels)
 
-	wm = perceptron(train_data,train_labels,1)
-	print avg_percep_clf(wm,test_data)
+	# Run 1,2,3 passes of perceptron, write training error
+	out = open('summary_output.txt','w')
 
+	# Training Error
+	
+	for i in xrange(1,4):
+		wm = perceptron(train_data,new_train_labels,i)
+		percep_labels = percep_orig_clf(wm,train_data)
+		voted_percep_labels = voted_percep_clf(wm,train_data)
+		avg_percep_labels = avg_percep_clf(wm,train_data)
+		out.write('Training Error' + '\n')
+		out.write('Pass %s' % i + '\n')
+		out.write('Perceptron: %s' % (calc_error(percep_labels,new_train_labels)) + '\n')
+		out.write('Voted: %s' % (calc_error(voted_percep_labels,new_train_labels)) + '\n')
+		out.write('Average: %s' % (calc_error(avg_percep_labels,new_train_labels)) + '\n')
+	
+	
+	# Test Error
+	"""
+	for i in xrange(1,4):
+		wm = perceptron(train_data,new_train_labels,i)
+		percep_labels = percep_orig_clf(wm,test_data)
+		voted_percep_labels = voted_percep_clf(wm,test_data)
+		avg_percep_labels = avg_percep_clf(wm,test_data)
+		out.write('Test Error' + '\n')
+		out.write('Pass %s' % i + '\n')
+		out.write('Perceptron: %s' % (calc_error(percep_labels,new_test_labels)) + '\n')
+		out.write('Voted: %s' % (calc_error(voted_percep_labels,new_test_labels)) + '\n')
+		out.write('Average: %s' % (calc_error(avg_percep_labels,new_test_labels)) + '\n')
+	"""
 if __name__ == '__main__':
 	main()
